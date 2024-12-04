fdd0556bff82c3d45ea390f8a3315f380643ac7c
San Valen <osoriovalentin5@gmail.com>
2024-12-04 16:15:17 -0500

    feat(page structure): add page structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languagesList = {
+  es: 'Español',
+  en: 'English',
+};
+
+export const labels = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Sobre mí',
+    'nav.projects': 'Proyectos',
+    'nav.contact': 'Contacto',
+    'home.title': 'Bienvenido a mi portfolio.',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact',
+    'home.title': 'Welcome to my portfolio.',
+  },
+};
