1c602978a9717b39208df329cc0d724d06652e7a
San Valen <osoriovalentin5@gmail.com>
2025-03-15 20:00:16 -0500

    refactor projects section

--- src/i18n/translations/projects.ts
+++ src/i18n/translations/projects.ts
@@ -5,6 +5,8 @@ export const projects = {
     'projects.esturesidencia.time': 'August 2023 - December 2023 (5 months)',
     'projects.pokestrategy.description': 'The application allows you to search for a pokemon by its name, when you select it, it will show you its basic information (skills and types). Also, it will show you in a simple way and in one place, the types against which it is strong, weak and neutral.',
     'projects.pokestrategy.time': 'September 2023 - October 2023 (2 months)',
+    'projects.tetris.description': 'Tetris is a puzzle game where you have to complete lines by rotating and moving the blocks.',
+    'projects.tetris.time': 'March 2023 (1 week)',
   },
   es: {
     'projects.see_code': 'Ver código',
@@ -12,5 +14,7 @@ export const projects = {
     'projects.esturesidencia.time': 'Agosto 2023 - Diciembre 2023 (5 meses)',
     'projects.pokestrategy.description': 'La aplicación te permite buscar un pokemon por su nombre, cuando lo selecciones, te mostrará su información básica (habilidades y tipos). Además, te mostrará de forma sencilla y en un solo lugar, los tipos contra los que es fuerte, débil y neutral.',
     'projects.pokestrategy.time': 'Septiembre 2023 - Octubre 2023 (2 meses)',
+    'projects.tetris.description': 'Tetris es un juego de puzzle donde tienes que completar líneas girando y moviendo los bloques.',
+    'projects.tetris.time': 'Marzo 2023 (1 semana)',
   },
 }
