3be9ba59ec77bf6be234284385ad571105a0f521
San Valen <osoriovalentin5@gmail.com>
2025-03-16 23:05:43 -0500

    add SEO config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	site: 'https://sanvalenjs.netlify.app/',
+	integrations: [
+		tailwind(),
+		sitemap({
+			i18n: {
+				defaultLocale: 'es',
+				locales: {
+					es: 'es-ES',
+					en: 'en-US'
+				}
+			}
+		})
+	],
 	i18n: {
 		defaultLocale: 'es',
 		locales: ['es', 'en'],
